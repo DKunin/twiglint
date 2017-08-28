372baa558da8d047b9fe74b5e838e0b0a0c2e0cb
Dmitry Kunin <dkunin@avito.ru>
2017-08-28 17:36:20 +0300

    Added failing tests for edge cases

--- test/cli/cli.spec.js
+++ test/cli/cli.spec.js
@@ -7,16 +7,18 @@ const promiseExec = util.promisify(exec);
 
 test('cli error', async t => {
     const errors = promiseExec('./index.js ./test/cli/cli-escaped-quotes.twig');
+
     t.deepEqual(await errors, {
-        stdout: '\n./test/cli/cli-escaped-quotes.twig\n  2:0  warning  "title" used, but not declared  no-undeclared-vars\n\n✖ 1 problem (0 errors, 1 warning)\n\n',
+        stdout: 'expression include "someItem.html" with {\n    title: title|replace({\'\\\\\': \'a\'})\n} only\n\n./test/cli/cli-escaped-quotes.twig\n  2:0  warning  "title" used, but not declared  no-undeclared-vars\n\n✖ 1 problem (0 errors, 1 warning)\n\n',
         stderr: ''
     });
 });
 
 test('cli parse error ', async t => {
     const errors = promiseExec('./index.js ./test/cli/cli-parser-error.twig');
+
     t.deepEqual(await errors, {
-        stdout: '\n./test/cli/cli-parser-error.twig\n  1:0  error    "br" is a self closing tag and should not be closed explicitly  no-closed-self-closing-tags\n  2:0  warning  "key" used, but not declared                                    no-undeclared-vars\n  3:0  warning  "category" used, but not declared                               no-undeclared-vars\n\n✖ 3 problems (1 error, 2 warnings)\n\n',
+        stdout: 'expression for key, value in {\n    \'Категория\': category\n}\nexpression endfor\n\n./test/cli/cli-parser-error.twig\n  1:0  error    "br" is a self closing tag and should not be closed explicitly  no-closed-self-closing-tags\n  2:0  warning  "key" used, but not declared                                    no-undeclared-vars\n  3:0  warning  "category" used, but not declared                               no-undeclared-vars\n\n✖ 3 problems (1 error, 2 warnings)\n\n',
         stderr: ''
     });
 });
@@ -25,9 +27,32 @@ test('cli only wothout with ', async t => {
     const errors = promiseExec(
         './index.js ./test/cli/cli-variable-follow.twig'
     );
-    // errors.then(res => console.log(JSON.stringify(res)));
+
     t.deepEqual(await errors, {
-        stdout: '\n./test/cli/cli-variable-follow.twig\n  1:0  warning  Use only to reduce amount of variables inside of template  no-only\n  1:0  warning  "only" used, but not declared                              no-undeclared-vars\n\n✖ 2 problems (0 errors, 2 warnings)\n\n',
+        stdout: 'expression include \'some.html\' only\n\n./test/cli/cli-variable-follow.twig\n  1:0  warning  Use only to reduce amount of variables inside of template  no-only\n  1:0  warning  "only" used, but not declared                              no-undeclared-vars\n\n✖ 2 problems (0 errors, 2 warnings)\n\n',
         stderr: ''
     });
+});
+
+test('cli constants parse', async t => {
+    const errors = promiseExec(
+        './index.js ./test/cli/cli-constants-parse.twig'
+    );
+    t.deepEqual(await errors, {});
+});
+
+test('parser error 2', async t => {
+    const errors = promiseExec(
+        './index.js ./test/cli/cli-parser-error-2.twig'
+    );
+    // errors.then(res => console.log(JSON.stringify(res)));
+    t.deepEqual(await errors, {});
+});
+
+test('cli-array-include-error', async t => {
+    const errors = promiseExec(
+        './index.js ./test/cli/cli-array-include-error.twig'
+    );
+    // errors.then(res => console.log(JSON.stringify(res)));
+    t.deepEqual(await errors, {});
 });
